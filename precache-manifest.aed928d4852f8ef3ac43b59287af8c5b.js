efa1928575a49685be260c9b92bfb0536a6e5a1a
michalniem <michal.niemiec@314t.com>
2020-05-11 12:08:53 +0200

    Updates

---
+++ precache-manifest.aed928d4852f8ef3ac43b59287af8c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7906a61df5414f7a55bfd945f48288e5",
+    "url": "/localhost_academy/index.html"
+  },
+  {
+    "revision": "4e9b5d333415337995aa",
+    "url": "/localhost_academy/static/css/main.4f818fc2.chunk.css"
+  },
+  {
+    "revision": "7ab48a83bccd3d46b559",
+    "url": "/localhost_academy/static/js/2.10623b03.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/localhost_academy/static/js/2.10623b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9b5d333415337995aa",
+    "url": "/localhost_academy/static/js/main.6296c330.chunk.js"
+  },
+  {
+    "revision": "2da463facdadf138c0c1",
+    "url": "/localhost_academy/static/js/runtime-main.1bde0c15.js"
+  }
+]);
