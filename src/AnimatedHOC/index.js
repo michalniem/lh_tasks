5428a73b77197e139d9e24b1472fb552a260fbe7
michalniem <michal.niemiec@314t.com>
2020-05-13 15:22:44 +0200

    Final version

--- src/AnimatedHOC/index.js
+++ src/AnimatedHOC/index.js
@@ -8,40 +8,63 @@ import WithAnimation from "./WithAnimation";
 function SomeComponent() {
   return (
     <div className="animatedElement">
-      I'm created by WithAnimation Higher Order Component.
+      I'm created by WithAnimation Higher Order Component. I have fade X
+      animation.
     </div>
   );
 }
 
-const fadeMotionOptions = {
+function SomeAnotherComponent() {
+  return (
+    <div className="animatedElement">
+      I'm created by WithAnimation Higher Order Component. I have default
+      animation.
+    </div>
+  );
+}
+
+const fadeXMotionOptions = {
   enter: { x: 0, opacity: 1 },
   exit: { x: -30, opacity: 0 },
   transition: { duration: 1 },
 };
 
-const SomeComponentWithAnimation = WithAnimation(SomeComponent)({
-  animation: fadeMotionOptions
-});
+const fadeYMotionOptions = {
+  enter: { y: 0, opacity: 1 },
+  exit: { y: -30, opacity: 0 },
+  transition: { duration: 1 },
+};
+
+const SomeComponentWithFadeAnimation = WithAnimation(SomeComponent)(
+  fadeXMotionOptions
+);
+
+const SomeComponentWithDefaultAnimation = WithAnimation(SomeAnotherComponent)();
 
 export default function index() {
   return (
     <>
       <div style={{ height: "100vh" }} />
-      <div style={{ minHeight: "500px" }}>
-        <SomeComponentWithAnimation />
-        <Animated animation={fadeMotionOptions}>
-          <div className="animatedElement">
-            I'm wrapped into Animated component and render as children. I have
-            fade in from left, fade out from right animation.
-          </div>
-        </Animated>
-        <Animated>
-          <div className="animatedElement">
-            I'm wrapped into Animated component and render as children. I have
-            default animation.
-          </div>
-        </Animated>
-      </div>
+      <SomeComponentWithFadeAnimation />
+      <SomeComponentWithDefaultAnimation />
+      <Animated animation={fadeXMotionOptions}>
+        <div className="animatedElement">
+          I'm wrapped into Animated component and render as children. I have
+          fade X animation.
+        </div>
+      </Animated>
+      <Animated>
+        <div className="animatedElement">
+          I'm wrapped into Animated component and render as children. I have
+          default animation.
+        </div>
+      </Animated>
+      <Animated animation={fadeYMotionOptions}>
+        <div className="animatedElement">
+          I'm wrapped into Animated component and render as children. I have
+          fade Y animation.
+        </div>
+      </Animated>
       <div style={{ height: "100vh" }} />
     </>
   );
