b030badfaf87e9ef0b1109839415b8a655dc122d
michalniem <michal.niemiec@314t.com>
2020-06-01 15:46:10 +0200

    Fixed tests

--- src/MultilangComponent/TranslatedText/index.js
+++ src/MultilangComponent/TranslatedText/index.js
@@ -8,6 +8,12 @@ function TranslatedText({ id, defaultText, children }) {
   const { translations } = useTranslations();
   const translatedText = id ? getObjectKeyByPath(id, translations) : defaultText;
 
+  // const ModifiedChildren = React.Children.map(children, (child, key) => {
+  //   return React.cloneElement(child, {
+  //     translatedText
+  //   })
+  // })
+
   return children ? children(translatedText) : <span>{translatedText}</span>;
 }
 
--- src/MultilangComponent/TranslatedText/index.test.js
+++ src/MultilangComponent/TranslatedText/index.test.js
@@ -6,25 +6,28 @@ import TranslatedText from "./index";
 import translations, { defaultLang } from "../../translations/config";
 
 describe("TranslatedText", () => {
-  const setupWrapper = ({ id, defaultText, children } = {}) => {
-    const Component = children ? (
-      <TranslatedText id={id} defaultText={defaultText}>
-        {children}
-      </TranslatedText>
-    ) : (
-      <TranslatedText id={id} defaultText={defaultText} />
+  const setupWrapper = (props) => {
+    return mount(
+      <TranslationsProvider>
+        <span data-test-id="translatedText_container">
+          <TranslatedText {...props} />
+        </span>
+      </TranslationsProvider>
     );
-    return mount(<TranslationsProvider><span data-test-id="translatedText_container">{Component}</span></TranslationsProvider>);
   };
 
   test("should return empty span if default text and id is not defined", () => {
     const wrapper = setupWrapper();
-    expect(wrapper.find("[data-test-id='translatedText_container'] span").text()).toEqual("");
+    expect(
+      wrapper.find("[data-test-id='translatedText_container'] span").text()
+    ).toEqual("");
   });
 
   test("should return span with default text if id is not defined", () => {
     const wrapper = setupWrapper({ defaultText: "DefaultText" });
-    expect(wrapper.find("[data-test-id='translatedText_container'] span").text()).toEqual("DefaultText");
+    expect(
+      wrapper.find("[data-test-id='translatedText_container'] span").text()
+    ).toEqual("DefaultText");
   });
 
   test("should return span with proper translation in default language", () => {
@@ -32,17 +35,21 @@ describe("TranslatedText", () => {
       id: "attention.title",
       defaultText: "DefaultText",
     });
-    expect(wrapper.find("[data-test-id='translatedText_container'] span").text()).toEqual(
-      translations[defaultLang].attention.title
-    );
+    expect(
+      wrapper.find("[data-test-id='translatedText_container'] span").text()
+    ).toEqual(translations[defaultLang].attention.title);
   });
 
   test("should return proper translation as plain text if component is rendered as render children", () => {
     const wrapper = setupWrapper({
       id: "attention.title",
       defaultText: "DefaultText",
-      children: text => `Translated text as render child: ${text}`
+      children: (text) => `Translated text as render child: ${text}`,
     });
-    expect(wrapper.find("[data-test-id='translatedText_container']").text()).toEqual(`Translated text as render child: ${translations[defaultLang].attention.title}`);
+    expect(
+      wrapper.find("[data-test-id='translatedText_container']").text()
+    ).toEqual(
+      `Translated text as render child: ${translations[defaultLang].attention.title}`
+    );
   });
 });
